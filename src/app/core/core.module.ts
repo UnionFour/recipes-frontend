d83e176a345f4162743cb7d092dad1d47a7a2969
Anastasia <fioB7@yandex.ru>
2023-10-22 20:00:32 +0500

    переход на одинарные ковычки в .editorconfg

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,5 +1,5 @@
-import { SharedModule } from '../shared/shared.module';
 import { NgModule, Optional, SkipSelf } from '@angular/core';
+import { SharedModule } from '../shared/shared.module';
 
 @NgModule({
     imports: [
