b083501663e17bcab92a69fb58fbc1058402d395
a.demina <ademina@tradedealer.ru>
2023-11-19 18:29:09 +0500

    recipes list

--- src/app/core/mocks/recipes.mock.ts
+++ src/app/core/mocks/recipes.mock.ts
@@ -148,5 +148,229 @@ export const recipes: any[] = [
             },
         ],
         nutrition: 700
+    },
+    {
+        id: 1,
+        title: 'Чипотта Аргентина Мокачино',
+        cookingMinutes: 125,
+        image: 'https://spoonacular.com/recipeImages/715544-312x231.jpg',
+        vegetarian: false,
+        vegan: true,
+        glutenFree: true,
+        dairyFree: true,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 234,
+                name: 'Морковь'
+            },
+            {
+                id: 235,
+                name: 'Молоко'
+            },
+            {
+                id: 235,
+                name: 'Яйцо'
+            },
+            {
+                id: 235,
+                name: 'Зелёный перец'
+            },
+            {
+                id: 235,
+                name: 'Карамель'
+            },
+        ],
+        nutrition: 234
+    },
+    {
+        id: 2,
+        title: 'Чаукатта Моргино кросс',
+        cookingMinutes: 210,
+        image: 'https://spoonacular.com/recipeImages/715455-312x231.jpg',
+        vegetarian: false,
+        vegan: true,
+        glutenFree: false,
+        dairyFree: false,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 285,
+                name: 'Помидор'
+            },
+            {
+                id: 23,
+                name: 'Капуста'
+            },
+            {
+                id: 2123,
+                name: 'Хлеб'
+            },
+            {
+                id: 1234,
+                name: 'Печенье'
+            },
+            {
+                id: 21349,
+                name: 'Мука'
+            },
+        ],
+        nutrition: 567
+    },
+    {
+        id: 3,
+        title: 'Орауи Рикко ротто',
+        cookingMinutes: 45,
+        image: 'https://spoonacular.com/recipeImages/716431-312x231.jpg',
+        vegetarian: true,
+        vegan: true,
+        glutenFree: false,
+        dairyFree: false,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 11,
+                name: 'Сосиски'
+            },
+            {
+                id: 235,
+                name: 'Молоко'
+            },
+            {
+                id: 235,
+                name: 'Хлеб'
+            },
+        ],
+        nutrition: 234
+    },
+    {
+        id: 4,
+        title: 'Чогонно курио потто суп по бабушкиному рецепту',
+        cookingMinutes: 12,
+        image: 'https://spoonacular.com/recipeImages/715381-312x231.jpg',
+        vegetarian: true,
+        vegan: true,
+        glutenFree: true,
+        dairyFree: true,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 45,
+                name: 'Лапша'
+            },
+            {
+                id: 674,
+                name: 'Картофель'
+            },
+            {
+                id: 9342,
+                name: 'Сливочное масло'
+            },
+            {
+                id: 429,
+                name: 'Любовь'
+            },
+            {
+                id: 9814,
+                name: 'Ненависть'
+            },
+            {
+                id: 235,
+                name: 'Хлеб'
+            },
+            {
+                id: 235,
+                name: 'Красный перец'
+            },
+            {
+                id: 235,
+                name: 'Пудинг'
+            },
+        ],
+        nutrition: 700
+    },
+    {
+        id: 3,
+        title: 'Орауи Рикко ротто',
+        cookingMinutes: 45,
+        image: 'https://spoonacular.com/recipeImages/716431-312x231.jpg',
+        vegetarian: true,
+        vegan: true,
+        glutenFree: false,
+        dairyFree: false,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 11,
+                name: 'Сосиски'
+            },
+            {
+                id: 235,
+                name: 'Молоко'
+            },
+            {
+                id: 235,
+                name: 'Хлеб'
+            },
+        ],
+        nutrition: 234
+    },
+    {
+        id: 4,
+        title: 'Чогонно курио потто суп по бабушкиному рецепту',
+        cookingMinutes: 12,
+        image: 'https://spoonacular.com/recipeImages/715381-312x231.jpg',
+        vegetarian: true,
+        vegan: true,
+        glutenFree: true,
+        dairyFree: true,
+        veryHealthy: true,
+        cheap: true,
+        veryPopular: true,
+        ingredients: [
+            {
+                id: 45,
+                name: 'Лапша'
+            },
+            {
+                id: 674,
+                name: 'Картофель'
+            },
+            {
+                id: 9342,
+                name: 'Сливочное масло'
+            },
+            {
+                id: 429,
+                name: 'Любовь'
+            },
+            {
+                id: 9814,
+                name: 'Ненависть'
+            },
+            {
+                id: 235,
+                name: 'Хлеб'
+            },
+            {
+                id: 235,
+                name: 'Красный перец'
+            },
+            {
+                id: 235,
+                name: 'Пудинг'
+            },
+        ],
+        nutrition: 700
     }
 ];
