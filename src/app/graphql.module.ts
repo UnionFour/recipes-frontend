d39a9b80c698c16a8e5c1f81aa32f30814b9d222
SemyonDuB <dubskikhsemyon@gmail.com>
2023-11-25 22:09:32 +0500

    Change graphql api url

--- src/app/graphql.module.ts
+++ src/app/graphql.module.ts
@@ -3,7 +3,7 @@ import { NgModule } from '@angular/core';
 import { ApolloModule, APOLLO_OPTIONS } from 'apollo-angular';
 import { ApolloClientOptions, InMemoryCache } from '@apollo/client/core';
 
-export const uri = 'http://localhost:5229/graphql'; // <-- add the URL of the GraphQL server here
+export const uri = 'http://77.223.96.15/graphql'; // <-- add the URL of the GraphQL server here
 export function createApollo(httpLink: HttpLink): ApolloClientOptions<any> {
     return {
         link: httpLink.create({ uri }),
