a554cf3991e229f2e9f09f70838d3bd25ed091de
Михаил <gmike103g@gmail.com>
2023-12-08 23:33:24 +0500

    small correction abstract class DestroyableComponent

--- src/app/shared/components/destroyable-component/destroyable.component.ts
+++ src/app/shared/components/destroyable-component/destroyable.component.ts
@@ -1,13 +1,13 @@
 import { Subject } from 'rxjs';
-import { Directive } from '@angular/core';
+import { Directive, OnDestroy } from '@angular/core';
 
 // родительский класс для отписок при уничтожении компоненты
 @Directive()
-export abstract class DestroyableComponent {
+export abstract class DestroyableComponent implements OnDestroy {
     protected destroy$: Subject<void> = new Subject<void>();
 
     public ngOnDestroy(): void {
-        this.destroy$.next(undefined);
+        this.destroy$.next();
         this.destroy$.complete();
     }
 }
