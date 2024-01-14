aa80966c987cc16f4828d9a87c9dd9322334aca8
Anastasia <fioB7@yandex.ru>
2024-01-14 19:19:22 +0500

    ingredients nav arrows

--- src/app/features/search-page/recipe-card/recipe-card.component.ts
+++ src/app/features/search-page/recipe-card/recipe-card.component.ts
@@ -1,4 +1,4 @@
-import { Component, Input, OnInit } from '@angular/core';
+import {AfterViewInit, Component, Input, OnInit} from '@angular/core';
 import { Recipe } from '../../../../gql/graphql';
 import { Category, categories } from '../../../core/models/filtering/category';
 
@@ -7,11 +7,15 @@ import { Category, categories } from '../../../core/models/filtering/category';
     templateUrl: './recipe-card.component.html',
     styleUrls: ['./recipe-card.component.scss']
 })
-export class RecipeCardComponent implements OnInit {
+export class RecipeCardComponent implements OnInit, AfterViewInit {
     @Input() public recipe!: Recipe;
 
     public recipeCategories: Category[] = [];
-    public index = 0;
+    public scrollIndex = 0;
+    public imaginaryIndex = 0;
+
+    private ingredientsCoords: number[] = [];
+    private endIngredientCoord: number = 0;
 
     ngOnInit() {
         categories.forEach((possibleCategory: Category) => {
@@ -23,5 +27,53 @@ export class RecipeCardComponent implements OnInit {
         });
     }
 
+    ngAfterViewInit() {
+        this.setIngredientCoordinates();
+    }
+
     protected readonly Math = Math;
+
+    public scrollIngredients(diff: number = 1) {
+        this.scrollIndex += diff;
+        const ingredients = document.getElementById(`ingredients${this.recipe.id}`);
+        if (ingredients) {
+            const ingredientsStart = ingredients.getBoundingClientRect().x;
+            const ingredientsWidth = ingredients.getBoundingClientRect().width + 20;
+            const xCoordination = this.ingredientsCoords[this.scrollIndex] - ingredientsStart;
+
+            let prevValue: number = this.ingredientsCoords[this.scrollIndex - diff] - ingredientsStart;
+            let remainder: number = 0;
+            const remainderArray = diff > 0 ?
+                this.ingredientsCoords.slice(this.scrollIndex) : this.ingredientsCoords.slice(0, this.scrollIndex);
+            remainderArray.push(this.endIngredientCoord);
+
+            remainderArray.forEach((curValue) => {
+                curValue -= ingredientsStart;
+                remainder += curValue - prevValue;
+                prevValue = curValue;
+            });
+
+            if (xCoordination >= 0) {
+                ingredients.scroll({left: xCoordination});
+                if (diff > 0 && remainder < ingredientsWidth) {
+                    this.imaginaryIndex = this.recipe.ingredients!.length;
+                } else if (diff < 0 && this.imaginaryIndex === this.recipe.ingredients!.length) {
+                    this.imaginaryIndex = this.scrollIndex;
+                }
+            }
+        }
+    }
+
+    private setIngredientCoordinates() {
+        this.recipe.ingredients?.forEach((ingredient, index) => {
+            const element = document.getElementById(`ingredient${index}`);
+            if (element) {
+                this.ingredientsCoords.push(element.getBoundingClientRect().x);
+
+                if (index === this.recipe.ingredients!.length - 1) {
+                    this.endIngredientCoord = element.getBoundingClientRect().right;
+                }
+            }
+        });
+    }
 }
